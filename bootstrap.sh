24ac8fe6daf5f4bbfa2724163a00419b71999960
Thomas SIMOES <simoes_t@etna-alternance.net>
2020-03-04 10:23:37 +0100

    fix:bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,13 @@
 #! /bin/bash
 
 cd ./front
+npm i
 npm link typescript
 npm link webpack
 webpack --config webpack-client.config.js
 webpack --config webpack-server.config.js
 export VIEW=`pwd`
 cd ../back
+npm i
+npm link typescript
 npm start
